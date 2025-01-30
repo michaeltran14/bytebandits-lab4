43afb838368535142cafd8faf2a5a96053dc33a4
tony-m-yang <tmyang@ualberta.ca>
2025-01-29 18:33:41 -0700

    resolved conflict and pulled from main

--- code/app/src/main/java/com/github/bytebandits/bytebandits_lab4/Shape.java
+++ code/app/src/main/java/com/github/bytebandits/bytebandits_lab4/Shape.java
@@ -3,7 +3,8 @@ package com.github.bytebandits.bytebandits_lab4;
 public abstract class Shape {
     private int x;
     private int y;
-    private String colour = "blue";
+    private String color = ".";
+
     public Shape(int x, int y) {
         this.x = x;
         this.y = y;
