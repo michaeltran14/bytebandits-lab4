e4095871a75a647f56786959e5091bf7c3c94bb4
Michael Tran <90705143+michaeltran14@users.noreply.github.com>
2025-01-29 18:26:06 -0700

    Update Shape.java

--- code/app/src/main/java/com/github/bytebandits/bytebandits_lab4/Shape.java
+++ code/app/src/main/java/com/github/bytebandits/bytebandits_lab4/Shape.java
@@ -3,7 +3,7 @@ package com.github.bytebandits.bytebandits_lab4;
 public abstract class Shape {
     private int x;
     private int y;
-    private String color = "black";
+    private String color = ".";
 
     public Shape(int x, int y) {
         this.x = x;
