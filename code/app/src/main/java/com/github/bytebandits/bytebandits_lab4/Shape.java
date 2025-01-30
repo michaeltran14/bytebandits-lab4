fe6b8a64e982f759693c856df1fd4dd1d875f722
rothman5 <rothman@ualberta.ca>
2025-01-29 18:20:45 -0700

    added private color attribute

--- code/app/src/main/java/com/github/bytebandits/bytebandits_lab4/Shape.java
+++ code/app/src/main/java/com/github/bytebandits/bytebandits_lab4/Shape.java
@@ -3,6 +3,7 @@ package com.github.bytebandits.bytebandits_lab4;
 public abstract class Shape {
     private int x;
     private int y;
+    private String color = "red";
 
     public Shape(int x, int y) {
         this.x = x;
